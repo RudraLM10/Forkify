c30835bab8dfc5b8d18a07565703f051bbcf5b0c
Rudra <rudralm10@gmail.com>
2024-07-28 12:11:28 +0530

    DONE

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '9807264a-1c29-4a5f-ab41-13b609761a43'; //KEY FROM AN API
+export const KEY = '475f1085-4829-4ff9-96cd-2fe3232ea5fa'; //KEY FROM AN API
 export const MODAL_CLOSE_SEC = 2.5;
